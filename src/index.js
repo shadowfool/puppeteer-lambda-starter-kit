4c95d5cae54e6762ad89f12405d68dd3e4fbb4fe
Aaron Freidus <afreidus@blueraster.com>
2017-12-24 01:56:48 -0500

    migrating code from shot_bot_lambda because I need prepackaged chrome to actualyl run pup onlambda, also converted all promises to async await

--- src/index.js
+++ src/index.js
@@ -1,19 +1,53 @@
 const setup = require('./starter-kit/setup');
+const request = require('request');
+const fs = require('fs');
+const {WebClient} = require('@slack/client');
+const web = new WebClient( process.env.SLACK_TOKEN );
 
 exports.handler = async (event, context, callback) => {
   // For keeping the browser launch
   context.callbackWaitsForEmptyEventLoop = false;
-  const browser = await setup.getBrowser();
-  exports.run(browser).then(
-    (result) => callback(null, result)
-  ).catch(
-    (err) => callback(err)
-  );
-};
 
-exports.run = async (browser) => {
+  const body = JSON.parse(event.body);
+  console.log('********The initial information from slack: ', event.body, '********');
+  const browser = await setup.getBrowser();
+  console.log('got browser');
+  const channel = body.event.channel;
+  const url = body.event.links[0].url;
   const page = await browser.newPage();
-  await page.goto('https://www.google.co.jp');
-  await page.close();
-  return 'done';
+  await page.goto(url);
+  console.log('********Redirected to login********');
+  await page.evaluate( (username, password) => {
+          const inputs = document.querySelectorAll('input');
+          inputs[0].value = username;
+          inputs[1].value = password;
+          document.querySelector('form').submit();
+   }, process.env.USERNAME, process.env.PASSWORD);
+
+   console.log('********Creds Inserted and Accepted********');
+   await page.waitForNavigation({
+    waitUntil: 'networkidle',
+    networkIdleTimeout: 500
+  });
+   console.log('********Preparing to Take Screenshot********');
+   await page.screenshot(
+        {
+          path: '/tmp/temp.jpeg',
+          type: 'jpeg',
+          quality: 50,
+        });
+    console.log('********Preparing to Upload Screenshot********');
+    console.log('the channel', channel);
+    web.files.upload('ss_bot.jpeg', {
+        file: fs.createReadStream(`/tmp/temp.jpeg`),
+        channels: [ channel ],
+        text: ' ',
+    })
+    .then( ( res ) => {
+      console.log('********Screenshot Uploaded********');
+    })
+    .catch( ( err ) => {
+        console.log(err);
+    });
+    callback(null, 'worked');
 };
