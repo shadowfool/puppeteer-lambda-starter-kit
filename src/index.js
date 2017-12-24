ea38534f7c646a84b2cb646449d5ff5968e48641
Aaron Freidus <afreidus@blueraster.com>
2017-12-24 10:36:47 -0500

    invocation down to 8200 ms g

--- src/index.js
+++ src/index.js
@@ -9,13 +9,21 @@ exports.handler = async (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
 
   const body = JSON.parse(event.body);
+
   console.log('********The initial information from slack: ', event.body, '********');
+
   const browser = await setup.getBrowser();
+
   console.log('got browser');
+
   const channel = body.event.channel;
+
   const url = body.event.links[0].url;
+
   const page = await browser.newPage();
+
   await page.goto(url);
+
   console.log('********Redirected to login********');
   await page.evaluate( (username, password) => {
           const inputs = document.querySelectorAll('input');
@@ -24,30 +32,35 @@ exports.handler = async (event, context, callback) => {
           document.querySelector('form').submit();
    }, process.env.USERNAME, process.env.PASSWORD);
 
-   console.log('********Creds Inserted and Accepted********');
-   await page.waitForNavigation({
+  console.log('********Creds Inserted and Accepted********');
+  
+  await page.waitForNavigation({
     waitUntil: 'networkidle',
     networkIdleTimeout: 500
   });
-   console.log('********Preparing to Take Screenshot********');
-   await page.screenshot(
-        {
-          path: '/tmp/temp.jpeg',
-          type: 'jpeg',
-          quality: 50,
-        });
-    console.log('********Preparing to Upload Screenshot********');
-    console.log('the channel', channel);
-    web.files.upload('ss_bot.jpeg', {
+
+  console.log('********Preparing to Take Screenshot********');
+  
+  await page.screenshot(
+    {
+      path: '/tmp/temp.jpeg',
+      type: 'jpeg',
+      quality: 50,
+    }
+  );
+  
+  console.log('********Preparing to Upload Screenshot********');
+    
+  web.files.upload('ss_bot.jpeg', {
         file: fs.createReadStream(`/tmp/temp.jpeg`),
         channels: [ channel ],
         text: ' ',
-    })
-    .then( ( res ) => {
-      console.log('********Screenshot Uploaded********');
-    })
-    .catch( ( err ) => {
+  })
+  .then( ( res ) => {
+    console.log('********Screenshot Uploaded********');
+    callback(null, { statusCode: 200 } );
+  })
+  .catch( ( err ) => {
         console.log(err);
-    });
-    callback(null, 'worked');
+  });
 };
